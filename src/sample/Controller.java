d2b35c472a4ca1bd32cb0662b054b007f9753121
jan-polaczek <jan.polaczek@interia.pl>
2021-01-18 21:15:11 +0100

    tryb prosty i inteligentny

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,13 +7,36 @@ import javafx.scene.shape.Rectangle;
 
 public class Controller {
     @FXML
+    public Button inside0;
+    @FXML
+    public Button inside1;
+    @FXML
+    public Button inside2;
+    @FXML
+    public Button inside3;
+    @FXML
+    public Button inside4;
+    @FXML
+    public Button outside0;
+    @FXML
+    public Button outside1;
+    @FXML
+    public Button outside2;
+    @FXML
+    public Button outside3;
+    @FXML
+    public Button outside4;
+    @FXML
     private Rectangle elevatorShape;
 
     private Elevator elevator;
 
     @FXML
     public void initialize() {
-        elevator = new Elevator(elevatorShape);
+        Button[] insideButtons = {inside0, inside1, inside2, inside3, inside4};
+        Button[] outsideButtons = {outside0, outside1, outside2, outside3, outside4};
+        elevator = new Elevator(elevatorShape, insideButtons, outsideButtons);
+        elevator.go();
     }
 
     @FXML
@@ -21,7 +44,24 @@ public class Controller {
         Button btn = (Button) e.getSource();
         String id = btn.getId();
         int floor = Integer.parseInt(id.substring(id.length() - 1));
-        elevator.setSpeedForFloor(floor);
-        elevator.go();
+        elevator.addFloorFromInside(floor);
+    }
+
+    @FXML
+    private void handleOutsidePress(MouseEvent e) {
+        Button btn = (Button) e.getSource();
+        String id = btn.getId();
+        int floor = Integer.parseInt(id.substring(id.length() - 1));
+        elevator.addFloorFromOutside(floor);
+    }
+
+    @FXML
+    private void handleSimpleModeChoice(MouseEvent e) {
+        elevator.setMode(Elevator.Mode.SIMPLE);
+    }
+
+    @FXML
+    private void handleSmartModeChoice(MouseEvent e) {
+        elevator.setMode(Elevator.Mode.SMART);
     }
 }
--- src/sample/Elevator.java
+++ src/sample/Elevator.java
@@ -1,62 +1,130 @@
 package sample;
 
 import javafx.application.Platform;
+import javafx.scene.control.Button;
 import javafx.scene.shape.Rectangle;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
 public class Elevator {
+    enum Mode {
+        SIMPLE,
+        SMART
+    };
+    private Mode mode = Mode.SIMPLE;
     private final int DEFAULT_SPEED = 5;
     private final int REFRESH_TIME = 50;
+    private final int WAIT_TIME = 1000;
+    private boolean shouldStopOverride = false;
     private int height = 0;
     private int speed = 0;
-    private int targetFloor;
-    private Rectangle shape;
+    private int targetFloor = 0;
+    private long dontMoveUntil = 0;
+    LinkedList<Integer> insideList = new LinkedList<>();
+    LinkedList<Integer> outsideList = new LinkedList<>();
+    private final Rectangle shape;
+    private final Button[] insideButtons;
+    private final Button[] outsideButtons;
     Timer timer = new Timer();
-    private boolean timerState = false;
 
-    public Elevator(Rectangle shape) {
+    public Elevator(Rectangle shape, Button[] insideButtons, Button[] outsideButtons) {
         this.shape = shape;
+        this.insideButtons = insideButtons;
+        this.outsideButtons = outsideButtons;
     }
 
-    public void go() {
-        int period = REFRESH_TIME;
-        if (timerState) {
-            timer.cancel();
-            timerState=false;
-        }
+    public void go()  {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
                 Platform.runLater(() -> {
-                    if(shouldStop()) {this.cancel();timerState=false;}
-                    else {
-                        timerState = true;
-                        move();
-                        if (period != REFRESH_TIME) {
-                            timer.cancel();
-                            timerState=false;
-                            go();
+                    if(!shouldStop()) {
+                        if (dontMoveUntil < System.currentTimeMillis()) {
+                            decideMove();
+                        }
+                    } else {
+                        int currentFloor = (int)translateHeightToFloor(height);
+                        setButtonToInactive(insideButtons, currentFloor);
+                        setButtonToInactive(outsideButtons, currentFloor);
+                        if (!insideList.isEmpty()) {
+                            dontMoveUntil = System.currentTimeMillis() + WAIT_TIME;
+                            targetFloor = insideList.removeLast();
+                        } else if (!outsideList.isEmpty()) {
+                            dontMoveUntil = System.currentTimeMillis() + WAIT_TIME;
+                            targetFloor = outsideList.removeLast();
                         }
                     }
                 });
             }
-        }, 0, period);
+        }, 0, REFRESH_TIME);
     }
 
-    public void move() {
+    private void decideMove() {
+        if (this.mode == Mode.SIMPLE) {
+            this.move();
+        } else if (this.mode == Mode.SMART) {
+            float currentFloorHeight = translateHeightToFloor(this.height);
+            if (currentFloorHeight % 1 == 0) {
+                int currentFloor = (int) currentFloorHeight;
+                if (this.insideList.contains(currentFloor) || this.outsideList.contains(currentFloor)) {
+                    removeAll(this.insideList, currentFloor);
+                    removeAll(this.outsideList, currentFloor);
+                    this.shouldStopOverride = true;
+                    this.insideList.add(this.targetFloor);
+                } else {
+                    this.move();
+                }
+            } else {
+                this.move();
+            }
+        }
+    }
+
+    private static void removeAll(LinkedList<Integer> list, Integer element) {
+        while (list.contains(element)) {
+            list.remove(element);
+        }
+    }
+
+    public void addFloorFromInside(int floor) {
+        if (this.insideList.isEmpty() || this.insideList.peek() != floor) {
+            this.insideList.push(floor);
+            this.setButtonToActive(this.insideButtons, floor);
+        }
+    }
+
+    private void setButtonToActive(Button[] list, int floor) {
+        list[floor].setStyle("-fx-background-color: #ffe500;  -fx-border-color: #aaaaaa;");
+    }
+
+    private void setButtonToInactive(Button[] list, int floor) {
+        list[floor].setStyle("-fx-background-color: #f0f0f0; -fx-border-color: #aaaaaa;");
+    }
+
+    public void addFloorFromOutside(int floor) {
+        if (this.outsideList.isEmpty() || this.outsideList.peek() != floor) {
+            this.outsideList.push(floor);
+            this.setButtonToActive(this.outsideButtons, floor);
+        }
+    }
+
+    private void move() {
+        this.setSpeedForFloor();
         this.setHeight(this.getHeight() + this.getSpeed());
     }
 
     public boolean shouldStop() {
-        return translateFloorToHeight(this.targetFloor) == this.height;
+        if (this.shouldStopOverride) {
+            this.shouldStopOverride = false;
+            return true;
+        } else {
+            return translateFloorToHeight(this.targetFloor) == this.height;
+        }
     }
 
-    public void setSpeedForFloor(int floor) {
-        int floorHeight = translateFloorToHeight(floor);
+    public void setSpeedForFloor() {
+        int floorHeight = translateFloorToHeight(this.targetFloor);
         this.speed = this.height < floorHeight ? DEFAULT_SPEED : -DEFAULT_SPEED;
-        this.targetFloor = floor;
     }
 
     public int getHeight() {
@@ -72,11 +140,15 @@ public class Elevator {
         return speed;
     }
 
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
-
     private static int translateFloorToHeight(int floor) {
         return - floor * 90;
+    }
+
+    private static float translateHeightToFloor(int height) {
+        return - (float)height / 90;
+    }
+
+    public void setMode(Mode mode) {
+        this.mode = mode;
     }
 }
